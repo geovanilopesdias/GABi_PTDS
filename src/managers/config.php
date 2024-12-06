34b9b9e92e98dd4a0f68e8d5cc3c74dcd2108c46
geovanilopesdias <geovani.phy.dias@gmail.com>
2024-12-06 16:18:45 -0300

    Rechange in db config file.

--- src/managers/config.php
+++ src/managers/config.php
@@ -1,7 +1,7 @@
 <?php
     return [
         'host' => 'autorack.proxy.rlwy.net',
-        'port' => '36341',
+        'port' => '5432',
         'db_name' => 'railway',
         'db_user' => 'postgres',
         'db_pass' => 'ENNmfKPveZLJTyqiFXqJAERabdUlAbaE'
